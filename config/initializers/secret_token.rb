b39f29b0b737c13598d026720eda3dbe2446b751
Xiaochun Shen <snowdonshen@gmail.com>
2011-07-03 19:54:31 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+S3food::Application.config.secret_token = 'e6c86afe75e5fc3fb4cc5ba8d6409988e23fde7be3a7bffc9d029ed33ad2e200d46894e070d02c9de67b20e64304c3c067007ad78ad9a68a285e9b297ffc38e4'
